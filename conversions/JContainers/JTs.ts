2ff7a1705ff888e677a96378bcb04b1fff3b6da0
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2021-11-22 16:18:12 -0600

    Add JTs
    A JContainers convenience library of functions.

---
+++ conversions/JContainers/JTs.ts
@@ -0,0 +1,43 @@
+/*
+This file was manually created.
+
+It adds functions that are only possible in Typescript.
+
+Some of these were made to deal with current SP limitations at 
+the moment of writing this, like array returning types from 
+SKSE functions not working.
+
+Others were made just for plain convenience.
+
+This file will be included in the official conversion release 
+once it has a good deal of useful functions.
+*/
+import * as JMap from "./JMap"
+
+/** JMap related functions. */
+export namespace JMapL {
+  /** Iterates over all JMap keys and executes a function `f` for each key.
+   *
+   * @param o Object handle for the JMap.
+   * @param f Function to execute for each key found.
+   * It accepts the `key` found and the object `o` as arguments.
+   *
+   * @example
+   * ForAllKeys(JValue.readFromFile(path), (armor, i) => {
+   *   const data = JMap.getObj(i, armor)
+   *   DoSomething(data)
+   * })
+   */
+  export function ForAllKeys(
+    o: number,
+    f: (key: string, object: number) => void
+  ) {
+    if (o === 0) return
+    let k = JMap.nextKey(o)
+
+    while (k !== "") {
+      f(k, o)
+      k = JMap.nextKey(o, k)
+    }
+  }
+}

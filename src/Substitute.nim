62c198cf7d1238468e00096eed7204955e695973
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2022-06-20 05:32:59 -0500

    Add more special manual instructions

--- src/Substitute.nim
+++ src/Substitute.nim
@@ -13,17 +13,21 @@ proc GetSubsStr(o: JsonNode): string =
   elif o.hasKey(fi): return o.fileFromkey()
   return ""
 
+proc SpecialOrSelf(rawRx: string, o: JsonNode, m: RegexMatch): string =
+  let spMode = m.groupFirstCapture(0, rawRx)
+  if spMode == "functionWithBody":
+    return r"(?isU)(?isU)export const $1.*endFunction" % o{"display"}.getStr("Lololol function is null")
+  elif spMode == "functionReturnsInt":
+    return r"(?isU)export const $1 =.*return\s+(\d+)\s+.*endFunction" % o{"display"}.getStr("Lololol function is null")
+  return rawRx
+
 # Returns substitution content, either verbatim or by special command
 proc SpecialOrSelf(o: JsonNode): string =
   let rawRx = o{"find"}.getStr()
   if rawRx == "": return ""
   var m: RegexMatch
   if not rawRx.match(re"--s:(.*)", m): return rawRx
-  let spMode = m.groupFirstCapture(0, rawRx)
-  if spMode == "functionWithBody":
-    return r"(?isU)(?isU)export const $1.*endFunction" % o{"display"}.getStr("Lololol function is null")
-  return rawRx
-  # r"(?isU)export const $1 =.*return\s+(\d+)\s+.*endFunction"
+  return rawRx.SpecialOrSelf(o, m)
 
 proc MakeSubstitutions*(txt, fileName: string): string = 
   var tx = txt
